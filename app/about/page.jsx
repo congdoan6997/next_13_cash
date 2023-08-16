0237502619e9c24c65cf8239b4eb46d17213ffd3
PhDBui <congdoan6997@gmail.com>
2023-08-16 07:11:05 +0700

    Init next_13_cash

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas qui dolor
+        doloremque blanditiis itaque asperiores debitis quis nulla vel! Quod
+        aspernatur ducimus vitae sit. Omnis commodi temporibus quas tenetur
+        officiis!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>My team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quas
+        incidunt numquam explicabo corporis alias aperiam cupiditate ab quis,
+        vero dignissimos ipsum provident, vel voluptates tenetur accusamus
+        commodi, delectus debitis?
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
