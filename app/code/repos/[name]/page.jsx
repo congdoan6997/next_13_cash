87070721a26638dba48bcd3fa01c3b73ff5daa8e
PhDBui <congdoan6997@gmail.com>
2023-08-16 14:16:01 +0700

    create dynamic routes with props in page

---
+++ app/code/repos/[name]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RepoPage = ({ params }) => {
+  return (
+    <div className="card">
+      <h2>{params.name}</h2>
+    </div>
+  );
+};
+
+export default RepoPage;
