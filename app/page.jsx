ebaadbd45f2bf97a2798cb45c80d03911812ba1c
PhDBui <congdoan6997@gmail.com>
2023-08-17 09:31:28 +0700

    fetching courses data

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import Courses from "./components/Courses";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Hello to my world</h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about/team">Team</Link>
-        </li>
-      </ul>
+      <h1>Hello to my project</h1>
+      <Courses></Courses>
     </div>
   );
 };
