3a852d197d8f6f897adeb8898140f194f6394542
PhDBui <congdoan6997@gmail.com>
2023-08-17 14:51:52 +0700

    add uuid and create method post api course

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,17 @@
 import { NextResponse } from "next/server";
+import {v4 as uuidv4} from "uuid";
 import courses from './data.json';
 
 export async function GET(request){
+    return NextResponse.json(courses);
+}
+
+export async function POST(request){
+    const {title, description, link, level} = await request.json();
+    const course = {
+        id: uuidv4(),
+        title,description ,link,level
+    }
+    courses.push(course);
     return NextResponse.json(courses);
 }
